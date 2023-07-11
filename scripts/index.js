35b91de6143016424d4323b6d71ab3c1aa921da5
Yarzar Tun (Lee) <yarzar56@gmail.com>
2023-07-11 10:29:45 -0700

    add first js

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+let initialCards = [
+  (card1 = {
+    cardName: "Yosimite Valley",
+    cardLink:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  }),
+  (card2 = {
+    cardName: "Lake Louise",
+    cardLink:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  }),
+  (card3 = {
+    cardName: "Bald Mountains",
+    cardLink:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  }),
+  (card4 = {
+    cardName: "Latemar",
+    cardLink:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  }),
+  (card5 = {
+    cardName: "Vanoise National Park",
+    cardLink:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  }),
+  (cards6 = {
+    cardName: "Lago di Braies",
+    cardLink:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  }),
+];
