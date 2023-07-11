2a95b068b11ef7c6d314e6886b53e4269fcaa3e9
Yarzar Tun (Lee) <yarzar56@gmail.com>
2023-07-11 11:02:34 -0700

    fixes for automated test

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,26 @@
 let initialCards = [
   {
-    cardName: "Yosimite Valley",
+    name: "Yosimite Valley",
     cardLink: "https://unsplash.com/photos/MNtnaKks2n8",
   },
   {
-    cardName: "Lake Louise",
-    cardLink:
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
   {
-    cardName: "Bald Mountains",
-    cardLink:
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
   {
-    cardName: "Latemar",
-    cardLink:
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
   {
-    cardName: "Vanoise National Park",
-    cardLink:
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
   {
-    cardName: "Lago di Braies",
-    cardLink:
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
